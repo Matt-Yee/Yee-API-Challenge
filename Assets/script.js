b8d666504e1b4074889851d300148621c464f1d2
Matt-Yee <matt.yee9@gmail.com>
2024-03-03 13:45:54 -0500

    first commit. established JS consts

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+const quizData = [{
+      question: "Question 1",
+      choices: ["A", "B", "C", "D"],
+      answer: 0,
+    },
+    {
+      question: "Question 2",
+      choices: ["A", "B", "C", "D"],
+      answer: 0,
+    },
+    // add more questions
+  ];
